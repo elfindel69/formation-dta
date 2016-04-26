f0d06f92a1be16ba7a9cc49a46c1722500f2bc12
Valentin Michel <elfindel69@gmail.com>
2016-04-26 14:22:02 +0200

    tp terminé

---
+++ operateurs-console/src/fr/operateurs/console/OperateursApp.java
@@ -0,0 +1,23 @@
+package fr.operateurs.console;
+
+import java.util.Scanner;
+
+public class OperateursApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("******** Application Operateurs *******");
+		System.out.println("Veuillez saisir le premier nombre");
+		double nb1 = sc.nextDouble();
+		System.out.println("Veuillez saisir le second nombre");
+		double nb2 = sc.nextDouble();
+		System.out.println(nb1+" + "+nb2+" = "+(nb1+nb2));
+		System.out.println(nb1+" - "+nb2+" = "+(nb1-nb2));
+		System.out.println(nb1+" * "+nb2+" = "+(nb1*nb2));
+		System.out.println(nb1+" / "+nb2+" = "+(nb1/nb2));
+		System.out.println(nb1+" % "+nb2+" = "+(nb1%nb2));
+		
+	}
+
+}
