286cb330df19bcced3e8157f7196c4f4722067dd
Valentin Michel <elfindel69@gmail.com>
2016-05-03 09:32:32 +0200

    modifs client

--- bankonet/src/com/bankonet/model/Client.java
+++ bankonet/src/com/bankonet/model/Client.java
@@ -1,34 +1,34 @@
 package com.bankonet.model;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import com.bankonet.exceptions.CompteNonTrouveException;
 
 /**
  * Modelise un client de bankonet.
  *
- * <p>Un client est caracterise par :
+ * <p>
+ * Un client est caracterise par :
  * <ul>
- * <li> son identifiant unique
- * <li> son nom
- * <li> son prenom
- * <li> la liste de ses comptes
+ * <li>son identifiant unique
+ * <li>son nom
+ * <li>son prenom
+ * <li>la liste de ses comptes
  * </ul>
  *
  * @author fguibert
  */
-public class Client implements Comparable{
+public class Client implements Comparable<Client> {
 	private int identifiant;
 	private String nom;
 	private String prenom;
-	
+
 	private List<CompteEpargne> compteEpargneList;
 	private List<CompteCourant> compteCourantList;
+
 	/**
 	 * @param nom
 	 * @param prenom
@@ -40,8 +40,8 @@ public class Client implements Comparable{
 		this.prenom = prenom;
 		this.identifiant = identifiant;
 	}
-	
-	public Client(int identifiant, String nom, String prenom, List<CompteCourant>cc, List<CompteEpargne>ce) {
+
+	public Client(int identifiant, String nom, String prenom, List<CompteCourant> cc, List<CompteEpargne> ce) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
@@ -49,105 +49,101 @@ public class Client implements Comparable{
 		this.compteEpargneList = ce;
 		this.compteCourantList = cc;
 	}
-	
+
 	public String toString() {
-		 return " ID  : "+this.getIdentifiant() +" - "+
-		    	" Nom : "+this.getNom()+" - "+
-		    	" Prénom : "+this.getPrenom()+ " - "+
-		    	"comptes courants: "+this.getNombreCompteCourant()+" - "+
-		    	"comptes epargne: "+this.getNombreCompteEpargne();
-		    		
-		
+		return " ID  : " + this.getIdentifiant() + " - " + " Nom : " + this.getNom() + " - " + " Prénom : "
+				+ this.getPrenom() + " - " + "comptes courants: " + this.getNombreCompteCourant() + " - "
+				+ "comptes epargne: " + this.getNombreCompteEpargne();
+
 	}
-	
-	public void creerCompte(Compte compte){
-		if(compte instanceof CompteEpargne){
+
+	public void creerCompte(Compte compte) {
+		if (compte instanceof CompteEpargne) {
 			compteEpargneList.add((CompteEpargne) compte);
 		}
-		if(compte instanceof CompteCourant){
+		if (compte instanceof CompteCourant) {
 			compteCourantList.add((CompteCourant) compte);
 		}
-		
+
 	}
-	
-	public void supprimerCompte(Compte compte){
-		if(compte instanceof CompteEpargne){
+
+	public void supprimerCompte(Compte compte) {
+		if (compte instanceof CompteEpargne) {
 			compteEpargneList.remove((CompteEpargne) compte);
 		}
-		if(compte instanceof CompteCourant){
+		if (compte instanceof CompteCourant) {
 			compteCourantList.remove((CompteCourant) compte);
 		}
-		
+
 	}
-	
-	public void supprimerCompte(String numero) throws CompteNonTrouveException{
+
+	public void supprimerCompte(String numero) throws CompteNonTrouveException {
 		Compte compte = retournerCompte(numero);
-		if(compte instanceof CompteEpargne){
+		if (compte instanceof CompteEpargne) {
 			compteEpargneList.remove((CompteEpargne) compte);
 		}
-		if(compte instanceof CompteCourant){
+		if (compte instanceof CompteCourant) {
 			compteCourantList.remove((CompteCourant) compte);
 		}
 	}
-	
-	public Compte retournerCompte(String numero) throws CompteNonTrouveException{
+
+	public Compte retournerCompte(String numero) throws CompteNonTrouveException {
 		Compte compte = null;
 		boolean trouve = false;
-		for (Compte c: getComptes()){
-			if(c.getIdentifiant() == Integer.parseInt(numero)){
+		for (Compte c : getComptes()) {
+			if (c.getIdentifiant() == Integer.parseInt(numero)) {
 				compte = c;
 				trouve = true;
 				break;
 			}
 		}
-		if(!trouve){
+		if (!trouve) {
 			throw new CompteNonTrouveException("Erreur ce compte n'existe pas");
 		}
 		return compte;
 	}
-	
-	public float calculerAvoirGLobal()
-	{
+
+	public float calculerAvoirGLobal() {
 		List<Compte> tousLesComptes = new ArrayList<>(this.getComptes());
 		float soldeTotal = 0;
-		for(Compte myC : tousLesComptes) {
+		for (Compte myC : tousLesComptes) {
 			soldeTotal += myC.getSolde();
 		}
 		return soldeTotal;
-		
+
 	}
-	
+
 	public List<Compte> getComptes() {
-	    ArrayList<Compte> compteList = new ArrayList<>();
-	    compteList.addAll(compteCourantList);
-	    compteList.addAll(compteEpargneList);
-	    return Collections.unmodifiableList(compteList);
+		ArrayList<Compte> compteList = new ArrayList<>();
+		compteList.addAll(compteCourantList);
+		compteList.addAll(compteEpargneList);
+		return Collections.unmodifiableList(compteList);
 
 	}
 
 	public Compte getCompte(int compteId) {
-	    List<Compte> compteList = this.getComptes();
-	    Iterator<Compte> compteIte = compteList.iterator();
-	    while (compteIte.hasNext()) {
-            Compte compte = (Compte) compteIte.next();
-            if (compteId == compte.getIdentifiant())
-                return compte;
-        }
-	    return null; 
+		List<Compte> compteList = this.getComptes();
+		Iterator<Compte> compteIte = compteList.iterator();
+		while (compteIte.hasNext()) {
+			Compte compte = (Compte) compteIte.next();
+			if (compteId == compte.getIdentifiant())
+				return compte;
+		}
+		return null;
 	}
-	
-	
-	
-	public int getNombreCompteCourant(){
+
+	public int getNombreCompteCourant() {
 		return compteCourantList.size();
 	}
-	
-	public int getNombreCompteEpargne(){
+
+	public int getNombreCompteEpargne() {
 		return compteEpargneList.size();
 	}
+
 	public int getIdentifiant() {
 		return identifiant;
 	}
+
 	/**
 	 * Retourne le nom du client.
 	 *
@@ -156,6 +152,7 @@ public class Client implements Comparable{
 	public String getNom() {
 		return nom;
 	}
+
 	/**
 	 * Retourne le prenom du client.
 	 * 
@@ -166,20 +163,16 @@ public class Client implements Comparable{
 	}
 
 	@Override
-	public int compareTo(Object o) {
-		int compare;
-		if(!(o instanceof Client)){
-			throw new ClassCastException();
-		}
-		Client cl = (Client) o;
-		if (this.identifiant > cl.identifiant){
+	public int compareTo(Client o) {
+		int compare = 0;
+		if (this.identifiant > o.identifiant) {
 			compare = 1;
-		}else if(this.identifiant < cl.identifiant){
-			compare= -1;
-		}else{
-			compare= 0;
+		} else if (this.identifiant < o.identifiant) {
+			compare = -1;
+		} else {
+			compare = 0;
 		}
-		
+
 		return compare;
 	}
 
