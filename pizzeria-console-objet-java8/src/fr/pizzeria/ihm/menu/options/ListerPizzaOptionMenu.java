a5416818b122730851a983aafd84a9ab81911f93
Valentin Michel <elfindel69@gmail.com>
2016-05-06 13:28:42 +0200

    initial commit

---
+++ pizzeria-console-objet-java8/src/fr/pizzeria/ihm/menu/options/ListerPizzaOptionMenu.java
@@ -0,0 +1,37 @@
+package fr.pizzeria.ihm.menu.options;
+
+import fr.pizzeria.dao.IPizzaDao;
+import fr.pizzeria.model.Pizza;
+
+/**
+ * Affichage de la liste des Pizzas
+ * @author Valentin
+ *
+ */
+public class ListerPizzaOptionMenu extends AbstractOptionMenu {
+
+	private static final String LISTER_PIZZA_MSG_NB_PIZZAS = " pizzas créées depuis l’initialisation du programme";
+	private static final String LISTER_PIZZA_MSG = "Liste des Pizzas";
+	private static final String LISTER_PIZZA_LIBELLE_MENU = "Lister pizzas";
+	
+	/**
+	 * Constructeur
+	 * @param pizzaDao - lien vers la DAO
+	 */
+	public ListerPizzaOptionMenu(IPizzaDao pizzaDao) {
+		super(LISTER_PIZZA_LIBELLE_MENU,pizzaDao);
+	}
+
+	/**
+	 * execution du menu
+	 * @return flag d'execution (true)
+	 */
+	@Override
+	public boolean execute() {
+		System.out.println(LISTER_PIZZA_MSG);
+		affichageListe();
+		System.out.println("-------" + Pizza.nbPizzas + LISTER_PIZZA_MSG_NB_PIZZAS);
+		return true;
+	}
+
+}
