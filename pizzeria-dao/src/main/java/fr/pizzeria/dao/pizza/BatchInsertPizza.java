2a75d95f12bc890699a0f45d6f97af6b4b6dbf5c
Valentin Michel <elfindel69@gmail.com>
2016-06-01 14:51:12 +0200

    Transaction import des pizzas

---
+++ pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/BatchInsertPizza.java
@@ -0,0 +1,37 @@
+package fr.pizzeria.dao.pizza;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.pizzeria.model.Pizza;
+
+@Repository
+@Lazy
+public class BatchInsertPizza {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public BatchInsertPizza(DataSource datasource, PlatformTransactionManager txManager) {
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void insertPizzas(List<Pizza> list) {
+		list.forEach(this::insertPizza);
+	}
+
+	private void insertPizza(Pizza p) {
+		String sql = "INSERT INTO pizza (nom,code,prix,categorie) VALUES (?,?,?,?)";
+		this.jdbcTemplate.update(sql, p.getNom(), p.getCode(), p.getPrix().doubleValue(), p.getCat().name());
+	}
+}
--- pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDaoJDBCImpl.java
+++ pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDaoJDBCImpl.java
@@ -1,11 +1,8 @@
 package fr.pizzeria.dao.pizza;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.apache.commons.collections4.ListUtils;
@@ -13,13 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.pizzeria.exceptions.DaoException;
-import fr.pizzeria.exceptions.DeletePizzaException;
-import fr.pizzeria.exceptions.SavePizzaException;
-import fr.pizzeria.exceptions.UpdatePizzaException;
 import fr.pizzeria.model.Pizza;
 
 @Repository
@@ -28,24 +21,12 @@ import fr.pizzeria.model.Pizza;
 public class PizzaDaoJDBCImpl implements IPizzaDao {
 
 	private JdbcTemplate jdbcTemplate;
+	private BatchInsertPizza batchInsertPizza;
 
 	@Autowired
-	public PizzaDaoJDBCImpl(DataSource datasource, PlatformTransactionManager txManager) {
+	public PizzaDaoJDBCImpl(DataSource datasource, BatchInsertPizza batchInsertPizza) {
 		this.jdbcTemplate = new JdbcTemplate(datasource);
-	}
-	
-	
-
-	private Map<String, Pizza> mapPizzas = new HashMap<>();
-
-	@PostConstruct
-	void init() {
-		try {
-			List<Pizza> pizzas = findAllPizzas();
-			pizzas.forEach(p -> mapPizzas.put(p.getCode(), p));
-		} catch (DaoException e) {
-			System.err.println(e);
-		}
+		this.batchInsertPizza = batchInsertPizza;
 	}
 
 	@Override
@@ -57,70 +38,30 @@ public class PizzaDaoJDBCImpl implements IPizzaDao {
 
 	@Override
 	public void savePizza(Pizza newPizza) throws DaoException {
-		if (!mapPizzas.containsKey(newPizza.getCode())) {
-			insertPizza(newPizza);
-		} else {
-			throw new SavePizzaException("ce code existe déjà");
-		}
-
+		String sql = "INSERT INTO pizza (nom,code,prix,categorie) VALUES (?,?,?,?)";
+		this.jdbcTemplate.update(sql, newPizza.getNom(), newPizza.getCode(), newPizza.getPrix().doubleValue(), newPizza.getCat().name());
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
-		if (mapPizzas.containsKey(codePizza)) {
-
-			String sql = "UPDATE pizza SET nom =?,code=?,prix=?,categorie=? WHERE code=?";
-			this.jdbcTemplate.update(sql, updatePizza.getNom(), updatePizza.getCode(),
-					updatePizza.getPrix().doubleValue(), updatePizza.getCat().name(), codePizza);
-			mapPizzas.put(codePizza, updatePizza);
-		} else {
-			throw new UpdatePizzaException("code non trouvé!");
-		}
-
+		String sql = "UPDATE pizza SET nom =?,code=?,prix=?,categorie=? WHERE code=?";
+		this.jdbcTemplate.update(sql, updatePizza.getNom(), updatePizza.getCode(), updatePizza.getPrix().doubleValue(),
+				updatePizza.getCat().name(), codePizza);
 	}
 
 	@Override
 	public void deletePizza(String codePizza) throws DaoException {
-		if (mapPizzas.containsKey(codePizza)) {
-
-			String sql = "DELETE FROM pizza WHERE code = ?";
-			this.jdbcTemplate.update(sql, codePizza);
-			mapPizzas.remove(codePizza);
-		} else {
-			throw new DeletePizzaException("code non trouvé!");
-		}
-
+		String sql = "DELETE FROM pizza WHERE code = ?";
+		this.jdbcTemplate.update(sql, codePizza);
 	}
 
 	@Override
+	@Transactional
 	public void importPizzas(List<Pizza> pizzas, int i) throws DaoException {
-
 		List<List<Pizza>> partition = ListUtils.partition(pizzas, i);
-
-		insertPizzas(partition);
-
+		partition.forEach(batchInsertPizza::insertPizzas);
 	}
 
-	private void insertPizzas(List<List<Pizza>> partition) throws UpdatePizzaException {
-		for (List<Pizza> list : partition) {
-
-			for (Pizza p : list) {
-				if (!mapPizzas.containsKey(p.getCode())) {
-					insertPizza(p);
-					System.out.println("pizza créée");
-				} else {
-					throw new UpdatePizzaException("pizza existante!");
-				}
-			}
-		}
-
-	}
-
-	private void insertPizza(Pizza p) {
-		String sql = "INSERT INTO pizza (nom,code,prix,categorie) VALUES (?,?,?,?)";
-		this.jdbcTemplate.update(sql, p.getNom(), p.getCode(), p.getPrix().doubleValue(), p.getCat().name());
-		mapPizzas.put(p.getCode(), p);
-	}
 
 	@Override
 	public Set<Pizza> findPizzasByCode(List<String> codes) throws DaoException {
@@ -130,7 +71,6 @@ public class PizzaDaoJDBCImpl implements IPizzaDao {
 	@Override
 	public Pizza findPizzaByCode(String code) {
 		String sql = "SELECT * FROM PIZZA WHERE code=?";
-
 		return this.jdbcTemplate.queryForObject(sql, new PizzaMapper(), code);
 	}
 
