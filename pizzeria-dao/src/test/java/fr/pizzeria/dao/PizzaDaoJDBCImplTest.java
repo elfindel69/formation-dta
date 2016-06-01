10e76a0d715b7ee9ff6527e63c216ee354b34a1b
Valentin Michel <elfindel69@gmail.com>
2016-06-01 17:22:22 +0200

    modifs

--- pizzeria-dao/src/test/java/fr/pizzeria/dao/PizzaDaoJDBCImplTest.java
+++ pizzeria-dao/src/test/java/fr/pizzeria/dao/PizzaDaoJDBCImplTest.java
@@ -7,6 +7,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,6 +19,7 @@ import fr.pizzeria.model.Pizza;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = PizzaDaoSpringTest.class)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class PizzaDaoJDBCImplTest {
 	@Autowired
 	private PizzaDaoJDBCImpl pizzaJDBC;
